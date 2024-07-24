4e14f7cd82f1dd1e010ff4ce5d7d26b87d00b4b7
darryl1975 <darryl1975@hotmail.com>
2024-07-24 11:21:17 +0800

    initial commit

---
+++ src/test/java/sg/edu/nus/iss/cicddemo/CicdDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.cicddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
